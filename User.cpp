c0e0b2d9ade7b159354fbc0e496a6e6fcfd6c6a4
Kacper ! <igor.grabowski@student.pk.edu.pl>
2026-06-07 21:01:58 +0200

    26_06_07_1_igor: Dodano logikę dla użytkownika: logowanie, tworzenie; dodano różne role użytkownika: Admin, Klient

--- User.cpp
+++ User.cpp
@@ -1,15 +1,28 @@
 #include "User.h"
 
-User::User(int id, std::string name, bool isPremium)
-    : id(id), name(name), isPremium(isPremium) {}
-    int User::getId() const {
-    return id;
+User::User(int id, const std::string& username, const std::string& password, UserRole role)
+    : id(id), username(username), password(password), role(role) {}
+
+int User::getId() const { 
+    return id; 
 }
 
-std::string User::getName() const {
-    return name;
+std::string User::getUsername() const { 
+    return username; 
 }
 
-bool User::getIsPremium() const {
-    return isPremium;
+std::string User::getPassword() const { 
+    return password; 
+}
+
+UserRole User::getRole() const { 
+    return role; 
+}
+
+std::string User::getRoleAsString() const {
+    return (role == UserRole::ADMIN) ? "ADMIN" : "CLIENT";
+}
+
+bool User::checkPassword(const std::string& inputPassword) const {
+    return password == inputPassword;
 }
--- User.h
+++ User.h
@@ -3,17 +3,31 @@
 
 #include <string>
 
-class User {
-public:
-    int id;
-    std::string name;
-    bool isPremium;
-
-    User(int id, std::string name, bool isPremium);
-    
-    int getId() const;
-    std::string getName() const;
-    bool getIsPremium() const;
+// Definicja kategorii użytkowników
+enum class UserRole {
+    CLIENT,
+    ADMIN
 };
 
-#endif
+class User {
+private:
+    int id;
+    std::string username;
+    std::string password;
+    UserRole role;
+
+public:
+    User(int id, const std::string& username, const std::string& password, UserRole role);
+
+    // Gettery
+    int getId() const;
+    std::string getUsername() const;
+    std::string getPassword() const;
+    UserRole getRole() const;
+    std::string getRoleAsString() const;
+
+    // Metoda weryfikacji hasła
+    bool checkPassword(const std::string& inputPassword) const;
+};
+
+#endif // USER_H
